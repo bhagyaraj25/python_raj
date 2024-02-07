911b7dda10bf2f0b7a6e4d9643d0ed16eb6d926d
bhagyaraj25 <159270343+bhagyaraj25@users.noreply.github.com>
2024-02-07 19:23:11 +0530

    account balance

---
+++ account balance.py
@@ -0,0 +1,14 @@
+account = 1000
+print("the total balance",account)
+
+account =  account+20
+print("credit of 20rs",account)
+
+account = account- 20
+print("debit of 10",account)
+
+print(10%account//10+account)
+
+print(account*1*10//100)
+print("after added 10% of interest  for 1 year =",account*1*10//100+account)
+
