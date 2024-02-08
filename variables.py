61aaa4b0246b08e8bafafa8a59da44e328adc290
bhagyaraj25 <159270343+bhagyaraj25@users.noreply.github.com>
2024-02-08 18:56:22 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,16 @@
+telugu   = 95
+english  = 90
+maths    = 85
+science  = 80
+kannada  = 70
+physics  = 95
+number_of_subjects = 6
+total_score = 600
+total_marks = telugu+english+maths+science+kannada+physics
+total_percentage = total_marks/total_score*100
+average = total_marks/number_of_subjects
+print(total_marks)
+print(total_percentage)
+print("raj scored 515 with 85.83%")
+print(average)
+
