76adc5ebc40997ccd72c35d739fa576b658414c5
kodavatikanti bhagyaraj <159270343+bhagyaraj25@users.noreply.github.com>
2024-02-12 20:56:33 +0530

    Add files via upload

---
+++ input function.py
@@ -0,0 +1,18 @@
+num1=input("enter the :")
+num2=input("enter the :")
+print(num1+num2)
+
+
+num1=int(input("enter the:"))
+num2=int(input("enter the :"))
+print(num1+num2)
+
+
+num1=float(input("enter the :"))
+num2=float(input("enter the :"))
+print(num1+num2)
+
+
+num1=complex(input("enter the :"))
+num2=complex(input("enter the:"))
+print(num1+num2)
