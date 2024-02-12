342fa78ac8c4e55c0d7fcd22e292cba23ae68ad2
kodavatikanti bhagyaraj <159270343+bhagyaraj25@users.noreply.github.com>
2024-02-12 18:52:32 +0530

    Add files via upload

---
+++ tables.py
@@ -0,0 +1,13 @@
+number1,number2,number3 = 25,50,75
+print(number1)
+print(number2)
+print(number3)
+
+num1=num2=num3 = 100,200,300
+print(f"num2 max is the highest with = {max(num3)}")
+print(f"num2 max is the highest with = {max(num3)}")
+print(f"num2 max is the highest with = {max(num3)}")
+
+
+a,b = 2,4
+print(a<b)
