e68296ed0e2c084c710d6d453a6ce2fdde41a1c4
kodavatikanti bhagyaraj <159270343+bhagyaraj25@users.noreply.github.com>
2024-02-12 20:54:36 +0530

    Add files via upload

---
+++ input add,sub,divi,multi.py
@@ -0,0 +1,27 @@
+a =int(input("enter the num1:"))
+b =int(input("enter the num2:"))
+print(f"addition of{a}+{b} = {a+b}")
+
+a =int(input("enter the num1:"))
+b =int(input("enter the num2:"))
+print(f"subtraction of {a}-{b}={a-b}")
+
+a =int(input("enter the num1:"))
+b =int(input("enter the num2:"))
+print(f"multiplication of {a}*{b}={a*b}")
+
+a =int(input("enter the num1:"))
+b =int(input("enter the num2:"))
+print(f"division of {a}/{b}={a/b}")
+
+a =int(input("enter the num1:"))
+b =int(input("enter the num2:"))
+print(f"modules of {a}%{b}={a%b}")
+
+a =int(input("enter the num1:"))
+b =int(input("enter the num2:"))
+print(f"floor division{a}//{b}={a//b}")
+
+a =int(input("enter the num1:"))
+b =int(input("enter the num2:"))
+print(f"power of {a}**{b}={a**b}")
