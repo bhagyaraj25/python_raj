b4532da817b64a79b25aff6affe8f8004a7fa1c8
kodavatikanti bhagyaraj <159270343+bhagyaraj25@users.noreply.github.com>
2024-02-13 19:04:20 +0530

    Add files via upload

---
+++ datatypes num.py
@@ -0,0 +1,50 @@
+#author = chandu
+#date   = 13-02-24
+#description = datatypes, numbers
+#batch  = 6.30 to 8.30
+
+a = 10
+b = 20
+a+=b
+print(a)
+
+a = 10
+b = 20
+a=a+b
+print(a)
+
+a = 10
+b = 15
+a-=b
+print(a)
+
+a = 10
+b = 15
+a=a-b
+print(a)
+
+a = 10
+b = 15
+a%=b
+print(a)
+
+a = 10
+b = 15
+a**=b
+print(a)
+
+a = 10
+b = 15
+a//=b
+print(a)
+
+a = 10
+b = 15
+a/=b
+print(a)
+
+a = 10
+b = 15
+a*=b
+print(a)
+
