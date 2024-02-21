0815af74d54f6f38fe26800d175915f0c1520933
kodavatikanti bhagyaraj <159270343+bhagyaraj25@users.noreply.github.com>
2024-02-21 20:19:17 +0530

    Add files via upload

---
+++ datatypes problems.py
@@ -0,0 +1,18 @@
+a = "sumanth"
+b = "sam"
+x= (a.capitalize())
+y= (b.capitalize())
+print(x,y)
+
+
+a = "sUMANTh"
+x = (a.swapcase())
+print(x)
+
+a = input("enter the strings:")
+x = input("enter the replace:")
+y = input("enter the word to replace with:")
+print(a.replace(x,y,1))
+
+
+
